c948d62bf25899d04c310dabc66af90d9b451e61
Khushi Shah <khushishah3502@gmail.com>
2025-07-24 11:11:11 +0530

    Added new class file in childBranch

---
+++ src/javaPackage/ChildBranchProj.java
@@ -0,0 +1,12 @@
+package javaPackage;
+
+public class ChildBranchProj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello child branch");
+
+	}
+
+}
